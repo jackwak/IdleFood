ed6e51a109e2362a99563c2ea02efdd8f7f4222c
jackwak <mehmetyanik53@hotmail.com>
2024-03-28 03:39:38 +0300

    refactor waiter class

--- Assets/Scripts/Waiter.cs
+++ Assets/Scripts/Waiter.cs
@@ -5,11 +5,13 @@ using UnityEngine.AI;
 
 public class Waiter : MonoBehaviour
 {
-    [HideInInspector]
-    public NavMeshAgent Agent;
-    [HideInInspector]
-    public Animator Animator;
+    [Header("References")]
+    [HideInInspector] public NavMeshAgent Agent;
+    [HideInInspector] public Animator Animator;
+    [HideInInspector] public ProgressBarController ProgressBarController;
     public GameObject SleepingGO;
+
+
     public bool HasFoodOnHand;
     public bool HasAnyCustomer;
     public bool HasAnyOrder;
@@ -17,10 +19,12 @@ public class Waiter : MonoBehaviour
     public Customer CurrentCustomer;
     public Order CurrentOrder;
 
+
     private void Awake()
     {
         Animator = GetComponent<Animator>();
         Agent = GetComponent<NavMeshAgent>();
+        ProgressBarController = GetComponent<ProgressBarController>();
     }
 
     public void SetWaiterAgentPosition(Vector3 position)
